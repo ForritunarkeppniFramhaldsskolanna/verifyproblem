cc92f6e2e9076f43741a400ce1ddfedf7278f704
Tagl <arnara14@ru.is>
2022-02-24 23:28:46 +0000

    still testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -l
 
+pwd
 ls -al
-#cd $GITHUB_WORKSPACE/problems
+cd $GITHUB_WORKSPACE
+pwd
+ls -al
 for problem in *; do
     if [[ -d $problem ]]; then
         if [ ! verifyproblem $1 ] ; then
