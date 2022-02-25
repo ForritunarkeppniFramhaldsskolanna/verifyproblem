8c9906ab55f421175244a4f31a8d9e96625436be
Tagl <arnara14@ru.is>
2022-02-25 12:28:09 +0000

    Only check changed problems

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 cd $GITHUB_WORKSPACE
 cd problems
-for problem in *; do
+git diff --name-only HEAD^ HEAD > _changed_files
+grep "^problems/" _changed_files | awk -F/ '{ print $2 }' | sort -u > _changed_problems
+for problem in $(cat _changed_problems); do
     if [[ -d $problem ]]; then
         verifyproblem $problem 2>&1 | tee output
         if [ ! -z "$(grep '^ERROR' output)" ] ; then
