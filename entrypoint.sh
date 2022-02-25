33fced5c33da90298f9161c7c89039d94c5309fc
Tagl <arnara14@ru.is>
2022-02-25 00:24:33 +0000

    Symlink for input_validators because docker image is old

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ cd $GITHUB_WORKSPACE
 cd problems
 for problem in *; do
     if [[ -d $problem ]]; then
+        ln -s $problem/input_validators $problem/input_format_validators # Remove this when docker image is updated
         verifyproblem $problem 2>&1 | tee output
         if [ ! -z "$(grep '^ERROR' output)" ] ; then
             exit 1
