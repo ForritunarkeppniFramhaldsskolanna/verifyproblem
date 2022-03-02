ac66642ee58a2ab08ef4dad08be8ce2c0f1fe73f
Tagl <arnara14@ru.is>
2022-03-02 13:20:13 +0000

    Generate pdfs and clearer errors/warnings

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,23 @@
 set -e
 cd $GITHUB_WORKSPACE
 cd problems
+result=0
 for problem in $(cat ../CHANGED_PROBLEMS); do
     if [[ -d $problem ]]; then
         verifyproblem $problem 2>&1 | tee output
-        if [ ! -z "$(grep '^ERROR' output)" ] ; then
-            exit 1
-        fi
+        while read -r line ; do
+            echo "::error title=Error while verifying problem $problem::$line"
+            result=1
+        done < <(grep '^ERROR' output)
+        while read -r line ; do
+            echo "::warning title=Warning while verifying problem $problem::$line"
+            result=1
+        done < <(grep '^WARNING' output)
         rm output
+        if ! problem2pdf $problem ; then
+            echo "::error title=Error generating pdf for problem $problem::"
+            result=1
+        fi
     fi
 done
+exit $result
