5a5eb06a1cc3750c5701ffa243c054decc8807fc
Tagl <arnara14@ru.is>
2023-01-07 18:51:53 +0000

    Turns out you actually have to provide the input for programs that depend on input for them to work properly

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ for problem in $(cat ../CHANGED_PROBLEMS); do
     if [[ -d $problem ]]; then
         verifyproblem $problem 2>&1 | tee output
         groupcount=$(ls -l $problem/data/secret | grep ^d | wc -l)
-        if ! pypy3 /check_verifyproblem_output.py $problem $groupcount ; then
+        if ! pypy3 /check_verifyproblem_output.py $problem $groupcount < output; then
             result=1
         fi
         rm output
